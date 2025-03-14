0d7f95345161a338186c5defa54e9fa56e5ceec9
Shanilka800@# <shanilka799@gmail.com>
2025-03-14 22:53:46 +0530

    update ui

--- components/home/SerchBar.tsx
+++ components/home/SerchBar.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { TextInput, View } from "react-native";
-import { Search } from "lucide-react-native"; 
+import Icon from "react-native-vector-icons/FontAwesome";
 
 function SearchBar() {
   return (
-    <View className="bg-gray-200 h-12 w-11/12 rounded-full absolute top-20 left-0 right-0 m-auto">
+    <View className="text-lg text-gray-700 text-left
+    bg-gray-200 h-12 w-11/12 rounded-full absolute top-20 left-0 right-0 m-auto">
       <TextInput
         placeholder="Search"
-        className="text-lg text-gray-700"
-      />
-      <Search size={24} color="gray" className="right-0"/>
+        />
+  <Icon name="search" size={20} color={'#666666'} className="top-24 left-72"/>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    // NOTE: Update this to include the paths to all of your component files.
-    content: ["./app/**/*.{js,jsx,ts,tsx}"],
-    presets: [require("nativewind/preset")],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./tabs/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
