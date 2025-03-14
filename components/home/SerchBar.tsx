83ae59c00b413552dbbc2d8d0567caf5eecf86dd
Shanilka800@# <shanilka799@gmail.com>
2025-03-14 23:12:37 +0530

    update SearchBar component css

--- components/home/SerchBar.tsx
+++ components/home/SerchBar.tsx
@@ -4,12 +4,21 @@ import Icon from "react-native-vector-icons/FontAwesome";
 
 function SearchBar() {
   return (
-    <View className="text-lg text-gray-700 text-left
-    bg-gray-200 h-12 w-11/12 rounded-full absolute top-20 left-0 right-0 m-auto">
+    <View
+      className="text-lg text-gray-700 text-left
+    bg-gray-200 h-8 w-11/12 rounded-full absolute top-14 left-0 right-0 m-auto"
+    >
       <TextInput
         placeholder="Search"
-        />
-  <Icon name="search" size={20} color={'#666666'} className="top-24 left-72"/>
+        className="text-sm text-gray-400 text-left
+    bg-gray-200 h-8 w-9/12 rounded-full absolute top-0 left-12 border-none outline-none focus:text-black"
+      />
+      <Icon
+        name="search"
+        size={16}
+        color={"#666666"}
+        className="top-0 bottom-0 m-auto -left-32"
+      />
     </View>
   );
 }
