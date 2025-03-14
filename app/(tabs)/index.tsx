5ab7ceab3dad460842f268e4599b36fd3aad17e5
Shanilka800@# <shanilka799@gmail.com>
2025-03-14 14:05:56 +0530

    remove nunUsed tsx file

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,28 +5,10 @@ import React from 'react';
 export default function HomeScreen() {
   return (
     <ScrollView className='bg-green-50 h-auto w-full'>
-      <View className='bg-slate-500 h-48 w-10/12 absolute'>
+      <View className='bg-slate-500 h-48 w-11/12 top-72 left-0 right-0 m-auto'>
 
       </View>
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
