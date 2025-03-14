31a913b217ca7e2e704774baa19b6aaf5d1cacac
Shanilka800@# <shanilka799@gmail.com>
2025-03-15 00:26:35 +0530

    create see All offer Button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { ScrollView,TouchableOpacity, View } from "react-native";
+import { ScrollView,Text,TouchableOpacity, View } from "react-native";
 import "../../global.css";
 import React from "react";
 import OfferCard from "@/components/home/OfferCard";
@@ -12,7 +12,11 @@ export default function HomeScreen() {
         <Icon name="shopping-cart" size={20}/>
       </TouchableOpacity>
       <SerchBar/>
-      <View className="top-28 w-11/12 left-0 right-0 m-auto rounded-3xl">
+      <View className="top-32 w-11/12 left-0 right-0 m-auto rounded-3xl">
+        <Text className="mb-4 font-bold ml-1">Special Offers</Text>
+        <TouchableOpacity className="absolute right-0 h-5 w-16 bg-green-100 rounded-full">
+          <Text className="mb-4 font-bold mr-1 text-green-600 text-center">See All</Text>
+        </TouchableOpacity>
         <OfferCard />
       </View>
     </ScrollView>
