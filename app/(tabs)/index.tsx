307487dedf4627a54228f7795727fd32cbc65097
Shanilka800@# <shanilka799@gmail.com>
2025-03-15 12:58:37 +0530

    update and fix bugs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,11 +7,16 @@ import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function HomeScreen() {
   return (
-    <ScrollView className="bg-white h-auto w-full">
-      <TouchableOpacity className="absolute bg-white w-6 h-6 items-center top-5 right-4">
-        <Icon name="shopping-cart" size={20}/>
-      </TouchableOpacity>
-      <SerchBar/>
+    <ScrollView className="bg-green-200 h-full w-full">
+      {/* SerchBar and OrderCard */}
+      <View>
+        <TouchableOpacity className="relative bg-white w-6 h-6 items-center top-5">
+          <Icon name="shopping-cart" size={20}/>
+        </TouchableOpacity>
+        <SerchBar/>
+      </View>
+
+      {/* Offer Card */}
       <View className="top-32 w-11/12 left-0 right-0 m-auto rounded-3xl">
         <Text className="mb-4 font-bold ml-1">Special Offers</Text>
         <TouchableOpacity className="absolute right-0 h-5 w-16 bg-green-100 rounded-full">
