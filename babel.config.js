17755a15f8843ea19e182fb4ab5e0f7518a9b42f
Shanilka800@# <shanilka799@gmail.com>
2025-03-15 11:23:16 +0530

    add nativwind

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: ['nativewind/babel'],
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: ["nativewind/babel"],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require("nativewind/preset")], // Corrected path
   content: [
     "./App.{js,jsx,ts,tsx}",
     "./app/**/*.{js,jsx,ts,tsx}",
     "./tabs/**/*.{js,jsx,ts,tsx}",
     "./screens/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
