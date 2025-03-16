bd895a6d8c6555179f18a555b2911207dd114d1d
Shanilka800@# <shanilka799@gmail.com>
2025-03-16 20:52:49 +0530

    create don cardPage item nott added view

--- app/CartPage.tsx
+++ app/CartPage.tsx
@@ -13,14 +13,32 @@ export default function CartPage() {
     <ScrollView className=" bg-white">
       {addToCartItem.length === 0 ? (
         <View className="bg-white h-96 w-full top-56">
-          <Image source={carticon} style={{width:120, height:120, left:-15, right:0, top:70, bottom:0, margin:'auto'}} />
+          <Image
+            source={carticon}
+            style={{
+              width: 120,
+              height: 120,
+              left: -15,
+              right: 0,
+              top: 95,
+              bottom: 0,
+              margin: "auto",
+              zIndex: 5,
+            }}
+          />
           <TouchableOpacity
             className="bg-black rounded left-0 right-0 m-auto w-40 h-8"
             onPress={() => navigation.goBack()}
+            style={{top:50}}
           >
-            <Text className="text-white text-center top-0 bottom-0 m-auto">Go To Shopping</Text>
+            <Text className="text-white text-center top-0 bottom-0 m-auto font-bold">
+              Go To Shopping
+            </Text>
           </TouchableOpacity>
-          <Text className="text-black">Cart is Empty</Text>
+          <Text className="text-black text-center left-0 right-0 m-auto w-10/12">
+            Cart is Empty. Save cart items here to get notified when the outlet
+            is available
+          </Text>
         </View>
       ) : (
         // Second View: Displayed if addToCartItem has items
