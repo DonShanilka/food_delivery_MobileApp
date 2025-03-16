123bd1b5025d0efe0689ccece054c620411126d9
Shanilka800@# <shanilka799@gmail.com>
2025-03-16 20:41:06 +0530

    update cart page ui

--- app/CartPage.tsx
+++ app/CartPage.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, Text, TouchableOpacity, ScrollView, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import Icon from "react-native-vector-icons/FontAwesome";
-const cartImage = require('../assets/images/shopingcart.jpg')
+
+const carticon = require("../assets/icons/cartIcon.png");
 
 export default function CartPage() {
   const navigation = useNavigation();
@@ -12,15 +12,15 @@ export default function CartPage() {
   return (
     <ScrollView className=" bg-white">
       {addToCartItem.length === 0 ? (
-        <View className="bg-blue-300 h-96">
-          <Text className="text-white">First View - Cart is Empty</Text>
-          <Image source={cartImage} style={{width:180, height:180}} />
+        <View className="bg-white h-96 w-full top-56">
+          <Image source={carticon} style={{width:120, height:120, left:-15, right:0, top:70, bottom:0, margin:'auto'}} />
           <TouchableOpacity
-            className="bg-red-500 rounded top-0 w-10"
+            className="bg-black rounded left-0 right-0 m-auto w-40 h-8"
             onPress={() => navigation.goBack()}
           >
-            <Text className="text-white">Close</Text>
+            <Text className="text-white text-center top-0 bottom-0 m-auto">Go To Shopping</Text>
           </TouchableOpacity>
+          <Text className="text-black">Cart is Empty</Text>
         </View>
       ) : (
         // Second View: Displayed if addToCartItem has items
