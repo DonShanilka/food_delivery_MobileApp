3aef1d69e91df30606d93bc0c9e09fe978b2d9bf
Shanilka800@# <shanilka799@gmail.com>
2025-03-17 14:21:09 +0530

    update card item delete all buton css

--- components/cart/AddCartItem.tsx
+++ components/cart/AddCartItem.tsx
@@ -1,152 +1,172 @@
 import React from "react";
 import { TouchableOpacity, Image, Text, View, ScrollView } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
-import Ionicons from '@expo/vector-icons/Ionicons';
+import Ionicons from "@expo/vector-icons/Ionicons";
 import PlaceOrder from "./PlaceOrder";
 
 function AddCartItem() {
   const cartItems = [
-      {
-        id: 1,
-        name: "Margherita Pizza",
-        price: 15,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Bella Pizza",
-      },
-      {
-        id: 2,
-        name: "Sushi Platter",
-        price: 22,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Tokyo Delights",
-      },
-      {
-        id: 3,
-        name: "Spaghetti Carbonara",
-        price: 18,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Pasta Primo",
-      },
-      {
-        id: 4,
-        name: "Sushi Rolls",
-        price: 20,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Sushi Express",
-      },
-      {
-        id: 5,
-        name: "Cheeseburger",
-        price: 12,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Burger King",
-      },
-      {
-        id: 6,
-        name: "Ramen Noodles",
-        price: 16,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Ramen House",
-      },
-      {
-        id: 7,
-        name: "Margherita Pizza",
-        price: 17,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Pizza Roma",
-      },
-      {
-        id: 8,
-        name: "Tuna Sushi",
-        price: 24,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Sushi World",
-      },
-      {
-        id: 9,
-        name: "Margarita Pizza",
-        price: 14,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "The Pizza Co.",
-      },
-      {
-        id: 10,
-        name: "Sashimi",
-        price: 28,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Sushi Palace",
-      },
-      {
-        id: 11,
-        name: "Lasagna",
-        price: 19,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Italian Bistro",
-      },
-      {
-        id: 12,
-        name: "Sushi Bento",
-        price: 21,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Bento Box",
-      },
-      {
-        id: 13,
-        name: "Pepperoni Pizza",
-        price: 16,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Pizza Slice",
-      },
-      {
-        id: 14,
-        name: "Nigiri Sushi",
-        price: 25,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Sushi Delight",
-      },
-      {
-        id: 15,
-        name: "Fettuccine Alfredo",
-        price: 22,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Pasta Delight",
-      },
-      {
-        id: 16,
-        name: "Tempura Rolls",
-        price: 18,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Sushi Corner",
-      },
-      {
-        id: 17,
-        name: "Caprese Salad",
-        price: 13,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Healthy Eats",
-      },
-      {
-        id: 18,
-        name: "California Rolls",
-        price: 19,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Sushi Express",
-      },
-      {
-        id: 19,
-        name: "Seafood Pizza",
-        price: 21,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Pizza Delight",
-      },
-      {
-        id: 20,
-        name: "Dragon Rolls",
-        price: 26,
-        image: "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
-        shopName: "Sushi Fusion",
-      },
-    ];
+    {
+      id: 1,
+      name: "Margherita Pizza",
+      price: 15,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Bella Pizza",
+    },
+    {
+      id: 2,
+      name: "Sushi Platter",
+      price: 22,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Tokyo Delights",
+    },
+    {
+      id: 3,
+      name: "Spaghetti Carbonara",
+      price: 18,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Pasta Primo",
+    },
+    {
+      id: 4,
+      name: "Sushi Rolls",
+      price: 20,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Sushi Express",
+    },
+    {
+      id: 5,
+      name: "Cheeseburger",
+      price: 12,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Burger King",
+    },
+    {
+      id: 6,
+      name: "Ramen Noodles",
+      price: 16,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Ramen House",
+    },
+    {
+      id: 7,
+      name: "Margherita Pizza",
+      price: 17,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Pizza Roma",
+    },
+    {
+      id: 8,
+      name: "Tuna Sushi",
+      price: 24,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Sushi World",
+    },
+    {
+      id: 9,
+      name: "Margarita Pizza",
+      price: 14,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "The Pizza Co.",
+    },
+    {
+      id: 10,
+      name: "Sashimi",
+      price: 28,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Sushi Palace",
+    },
+    {
+      id: 11,
+      name: "Lasagna",
+      price: 19,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Italian Bistro",
+    },
+    {
+      id: 12,
+      name: "Sushi Bento",
+      price: 21,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Bento Box",
+    },
+    {
+      id: 13,
+      name: "Pepperoni Pizza",
+      price: 16,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Pizza Slice",
+    },
+    {
+      id: 14,
+      name: "Nigiri Sushi",
+      price: 25,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Sushi Delight",
+    },
+    {
+      id: 15,
+      name: "Fettuccine Alfredo",
+      price: 22,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Pasta Delight",
+    },
+    {
+      id: 16,
+      name: "Tempura Rolls",
+      price: 18,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Sushi Corner",
+    },
+    {
+      id: 17,
+      name: "Caprese Salad",
+      price: 13,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Healthy Eats",
+    },
+    {
+      id: 18,
+      name: "California Rolls",
+      price: 19,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Sushi Express",
+    },
+    {
+      id: 19,
+      name: "Seafood Pizza",
+      price: 21,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Pizza Delight",
+    },
+    {
+      id: 20,
+      name: "Dragon Rolls",
+      price: 26,
+      image:
+        "https://i.pinimg.com/236x/a9/fd/bd/a9fdbd00141682c6a9e848c43dbf4c5c.jpg",
+      shopName: "Sushi Fusion",
+    },
+  ];
 
   let subTotal: number = 0;
   cartItems.forEach((item) => {
@@ -155,48 +175,66 @@ function AddCartItem() {
 
   return (
     <>
-    {/* BackTo Home */}
-    <View className="bg-white w-11/12 h-16 absolute top-10 left-5 right-5">
-      <Ionicons name="trash-bin" size={24} color="red" className="absolute top-5 right-0"/>
-      <Ionicons name="arrow-back-outline" size={24} color="black" className="absolute top-5 bg-gray-100 rounded-full"/>
-      <Text className="top-4 left-12 font-bold text-2xl">Cart</Text>
-    </View>
+      {/* BackTo Home and Delete All Button */}
+      <View className="w-11/12 h-16 absolute top-10 left-5 right-5">
 
-    {/* Items */}
-    <ScrollView className="w-full absolute top-32 h-3/5 bg-white">
-      {cartItems.map((value) => (
-        <View
-          key={value.id}
-          className="h-32 w-11/12 left-0 right-0 m-auto mt-3 mb-1 rounded-lg border border-gray-200 bg-gray-100 p-3"
-        >
-          <Text className="ml-28 mt-2 font-bold">{value.name}</Text>
-          <Text className="ml-28 mt-1 text-gray-600">{value.shopName}</Text>
-          <Image
-            source={{ uri: value.image }}
-            className="w-20 h-16 absolute top-0 bottom-0 my-auto left-3"
+      {/* Delete All Button */}
+        <TouchableOpacity className="absolute top-0 right-0 w-12 h-16">
+          <Ionicons
+            name="trash-bin"
+            size={26}
+            color="red"
+            className="absolute top-4 right-1"
           />
-          <Text className="ml-28 mt-1 font-bold">${value.price}</Text>
+        </TouchableOpacity>
 
-          <TouchableOpacity className="absolute top-1 right-2">
-            <Icon name="remove" color={"#a6a6a6"} size={10}/>
-          </TouchableOpacity>
+        {/* BackToHome */}
+        <TouchableOpacity>
+          <Ionicons
+            name="arrow-back-outline"
+            size={24}
+            color="black"
+            className="absolute top-5 bg-gray-100 rounded-full"
+          />
+        </TouchableOpacity>
+        <Text className="top-4 left-12 font-bold text-2xl">Cart</Text>
+      </View>
 
-          {/* Quantity Control (Plus & Minus) */}
-          <View className="absolute flex-row items-center justify-center bottom-2 right-2">
-            <TouchableOpacity className="bg-emerald-500 w-6 h-6 rounded-full items-center justify-center">
-              <Icon name="minus" size={12} color="#fff" />
+      {/* Items */}
+      <ScrollView className="w-full absolute top-32 h-3/5 bg-white">
+        {cartItems.map((value) => (
+          <View
+            key={value.id}
+            className="h-32 w-11/12 left-0 right-0 m-auto mt-3 mb-1 rounded-lg border border-gray-200 bg-gray-100 p-3"
+          >
+            <Text className="ml-28 mt-2 font-bold">{value.name}</Text>
+            <Text className="ml-28 mt-1 text-gray-600">{value.shopName}</Text>
+            <Image
+              source={{ uri: value.image }}
+              className="w-20 h-16 absolute top-0 bottom-0 my-auto left-3"
+            />
+            <Text className="ml-28 mt-1 font-bold">${value.price}</Text>
+
+            <TouchableOpacity className="absolute top-1 right-2">
+              <Icon name="remove" color={"#a6a6a6"} size={10} />
             </TouchableOpacity>
-            <Text className="mx-4 font-bold">1</Text>
-            <TouchableOpacity className="bg-emerald-500 w-6 h-6 rounded-full items-center justify-center">
-              <Icon name="plus" size={12} color="#fff" />
-            </TouchableOpacity>
+
+            {/* Quantity Control (Plus & Minus) */}
+            <View className="absolute flex-row items-center justify-center bottom-2 right-2">
+              <TouchableOpacity className="bg-emerald-500 w-6 h-6 rounded-full items-center justify-center">
+                <Icon name="minus" size={12} color="#fff" />
+              </TouchableOpacity>
+              <Text className="mx-4 font-bold">1</Text>
+              <TouchableOpacity className="bg-emerald-500 w-6 h-6 rounded-full items-center justify-center">
+                <Icon name="plus" size={12} color="#fff" />
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-      ))}
-    </ScrollView>
+        ))}
+      </ScrollView>
 
-    {/* SubTotal */}
-    <PlaceOrder/>
+      {/* SubTotal */}
+      <PlaceOrder />
     </>
   );
 }
