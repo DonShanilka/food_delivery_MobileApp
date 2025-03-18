41422c0354302b340802c11269a92b463a7ab7ad
Shanilka800@# <shanilka799@gmail.com>
2025-03-18 16:29:46 +0530

    update serchbar css

--- components/home/SerchBar.tsx
+++ components/home/SerchBar.tsx
@@ -6,18 +6,18 @@ function SearchBar() {
   return (
     <View
       className="text-lg text-gray-700 text-left
-    bg-white h-11 w-full rounded-full absolute top-20 left-0 right-0 m-auto border-2 border-gray-200"
+    bg-white h-14 w-11/12 left-0 right-0 m-auto rounded-md top-0 border-2 border-gray-200"
     >
       <TextInput
         placeholder="Search"
         className="text-sm text-gray-400 text-left
-    bg-opacity-0 h-10 w-9/12 rounded-full absolute top-0 left-9 border-none outline-none focus:text-black z-10"
+    bg-opacity-0 h-10 w-9/12 rounded-full absolute top-2 left-9 border-none outline-none focus:text-black z-10"
       />
       <Icon
         name="search"
         size={16}
         color={"#666666"}
-        className="top-2 left-3"
+        className="top-4 left-3" 
       />
     </View>
   );
