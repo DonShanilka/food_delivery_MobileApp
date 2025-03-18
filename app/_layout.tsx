7667e9f57e5e3c0dacfb52c3b50cf51ea4c39945
Shanilka800@# <shanilka799@gmail.com>
2025-03-18 21:22:05 +0530

    create loginpage and signinpage and fix navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,26 @@
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
 import "react-native-reanimated";
 import React from "react";
+import { View, Text, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import SplashScreen from "../app/SplashScreen";
+import LoginScreen from "../app/LoginScreen";
+import SignUpScreen from "../app/SignUpScreen";
+import HomeScreen from "./(tabs)/index";
+import ProfileScreen from "./(tabs)/ProfileScreen ";
+import CartPage from "../app/CartPage";
+import FoodDetails from "../app/FoodDetails";
+import TabNavigator from "./(tabs)/_layout";
 
-// Create Stack Navigator
-// const StackNav = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
 
   if (!loaded) {
     return null;
@@ -29,11 +28,17 @@ export default function RootLayout() {
 
   return (
     <>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="CartPage" options={{ headerShown: false }} />
-        <Stack.Screen name="FoodDetails" options={{ headerShown: false }} />
-      </Stack>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* Auth Flow */}
+        <Stack.Screen name="Splash" component={SplashScreen}  options={{ headerShown: false }}/>
+        <Stack.Screen name="Login" component={LoginScreen}  options={{ headerShown: false }}/>
+        <Stack.Screen name="SignUp" component={SignUpScreen}  options={{ headerShown: false }}/>
+
+        {/* Main App Flow */}
+        <Stack.Screen name="Main" component={TabNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="CartPage" component={CartPage}  options={{ headerShown: false }}/>
+        <Stack.Screen name="FoodDetails" component={FoodDetails}  options={{ headerShown: false }}/>
+      </Stack.Navigator>
       <StatusBar />
     </>
   );
