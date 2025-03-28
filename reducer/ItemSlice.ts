98331547c2db7a372b7ef0460ea5db4e4c6b6e07
Shanilka800@# <shanilka799@gmail.com>
2025-03-28 16:54:31 +0530

    create itemSlice

--- reducer/ItemSlice.ts
+++ reducer/ItemSlice.ts
@@ -1,6 +1,7 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import axios from 'axios';
 import { Item } from '@/model/Item';
+import { act } from 'react';
 
 const initialState: Item[] = [];
 
@@ -14,5 +15,24 @@ export const fetchItems = createAsyncThunk('item/saveItem', async (itemData: Ite
     console.log("response: ", response.data);
   } catch (error) {
     console.log("Canot fetchItems: ", error);
+  }
+});
+
+const itemSlice = createSlice({
+  name: 'item',
+  initialState: initialState,
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchItems.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(fetchItems.rejected, (state, action) => {
+        console.log("Failed to Get Items", action.payload)
+      })
+      .addCase(fetchItems.pending, () => {
+        console.log("Fetching Item Pending ...")
+      })
   }
 })
