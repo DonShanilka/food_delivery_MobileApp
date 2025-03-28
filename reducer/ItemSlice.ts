c71f2896755a388b4b1483800012c178cd4bc67a
Shanilka800@# <shanilka799@gmail.com>
2025-03-28 16:42:43 +0530

    create fetchItems funtion in ItemSlice

--- reducer/ItemSlice.ts
+++ reducer/ItemSlice.ts
@@ -1,3 +1,18 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 import axios from 'axios';
 import { Item } from '@/model/Item';
+
+const initialState: Item[] = [];
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000',
+});
+
+export const fetchItems = createAsyncThunk('item/saveItem', async (itemData: Item) => {
+  try {
+    const response = await api.get('/api/item/getAllItem');
+    console.log("response: ", response.data);
+  } catch (error) {
+    console.log("Canot fetchItems: ", error);
+  }
+})
