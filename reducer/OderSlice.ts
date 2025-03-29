07c51ce2e01e3313944af8482eee40c96fe40e89
Shanilka800@# <shanilka799@gmail.com>
2025-03-29 23:47:31 +0530

    create orderSlice

--- reducer/OderSlice.ts
+++ reducer/OderSlice.ts
@@ -1,3 +1,23 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import { Item } from "../model/Item";
+import { Order } from "../model/Order";
+
+const initialState: Order[] = [];
+
+const api = axios.create({
+  baseURL: "http://192.168.1.101:3000", 
+});
+
+export const AddOrder = createAsyncThunk(
+  "order/addOrder",
+  async (orderData, { rejectWithValue }) => {
+    try {
+      const response = await api.post("/api/order/addOrder", orderData);
+      console.log("Order Added Successfully: ", response.data);
+      return response.data; // Return the response to the Redux store
+    } catch (error) {
+      console.error("Cannot add order: ", error);
+      return rejectWithValue(error.response?.data || "An error occurred while adding the order");
+    }
+  }
+);
